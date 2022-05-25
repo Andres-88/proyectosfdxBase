c852eece417f2158eff2801958f5c752c9086f81
Renzo Oldani <oldanirenzo@gmail.com>
2022-05-25 12:06:33 -0300

    Vuelta al inicio

--- R2D2PackRetri.py
+++ R2D2PackRetri.py
@@ -52,35 +52,35 @@ tree = ET.ElementTree(indent(root))
 tree.write('package.xml', xml_declaration=True, encoding='utf-8')
 
 ########## comentar desde de aqui si solo se busca el XML#################################
-# os.system("sfdx force:mdapi:retrieve -r ./ -u " + origen + " -k ./package.xml")
+os.system("sfdx force:mdapi:retrieve -r ./ -u " + origen + " -k ./package.xml")
 
 
-# print("Empiezo a listar ficheros")
-# with os.scandir('./') as ficheros:
-#     for fichero in ficheros:
-#         print(fichero.name)
-# print("Termino de listar ficheros")
+print("Empiezo a listar ficheros")
+with os.scandir('./') as ficheros:
+    for fichero in ficheros:
+        print(fichero.name)
+print("Termino de listar ficheros")
 
-# print("Inicio creacion UNPACKAGED")
+print("Inicio creacion UNPACKAGED")
 
-# with zipfile.ZipFile("./unpackaged.zip", "r") as zip_ref:
-#     zip_ref.extractall("./")
-# try:
-#     if os.listdir('./unpackaged/audience/'):
-#         os.system('rm -r ./unpackaged/audience/')
-# except:
-#     print('audience no existe')
-# try:
-#     if os.listdir('./unpackaged/customindex/'):
-#         os.system('rm -r ./unpackaged/customindex/')
-# except:
-#     print('customindex no existe')
-# try:
-#     if os.listdir('./unpackaged/uiObjectRelationConfigs/'):
-#         os.system('rm -r ./unpackaged/uiObjectRelationConfigs/')
-# except:
-#     print('uiObjectRelationConfigs no existe')
+with zipfile.ZipFile("./unpackaged.zip", "r") as zip_ref:
+    zip_ref.extractall("./")
+try:
+    if os.listdir('./unpackaged/audience/'):
+        os.system('rm -r ./unpackaged/audience/')
+except:
+    print('audience no existe')
+try:
+    if os.listdir('./unpackaged/customindex/'):
+        os.system('rm -r ./unpackaged/customindex/')
+except:
+    print('customindex no existe')
+try:
+    if os.listdir('./unpackaged/uiObjectRelationConfigs/'):
+        os.system('rm -r ./unpackaged/uiObjectRelationConfigs/')
+except:
+    print('uiObjectRelationConfigs no existe')
 
-# print("FIN creacion UNPACKAGED")
+print("FIN creacion UNPACKAGED")
 
-# os.system("sfdx force:mdapi:convert --rootdir ./unpackaged --outputdir ./Salesforce")
+os.system("sfdx force:mdapi:convert --rootdir ./unpackaged --outputdir ./Salesforce")
