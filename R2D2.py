fcbe13b53819b23173c00d89f979964eb262329b
Renzo Oldani <oldanirenzo@gmail.com>
2022-06-10 15:12:14 -0300

    Modified

--- R2D2.py
+++ R2D2.py
@@ -33,6 +33,34 @@ def convert():
     os.system("rm -r ./unpackages/unpackaged")
     time.sleep(1)
 
+def createBigXMLs(start=0, end=30,xmlVersion=1):
+    root = ET.Element('Package', {'xmlns': 'http://soap.sforce.com/2006/04/metadata'})
+    tipo = f.split('.')
+    tipos = ET.SubElement(root, 'types')
+    componentes = open('./data/'+f)
+    s = componentes.read()
+    members = json.loads(s)
+    if not isinstance(members, list):
+        members = json.loads(s)
+    not_necessary= ['CustomObjectTranslation.json', 'Workflow.json', 'InstalledPackage.json', 'KeywordLis.json', 'ModerationRule.json', 'WebLink.json', 'WorkflowFieldUpdate.json', 'UserCriteria.json', 'SiteDotCom.json', 'SharingRules.json', 'SharingGuestRule.json', 'Report.json','meta.json']
+    if not f in not_necessary:
+        for j in range(start,end):
+            if j == len(members):
+                break
+            metadata = ET.SubElement(tipos, 'members')
+            metadata.text = members[j]['fullName']
+        name = ET.SubElement(tipos, 'name')
+        name.text = tipo[0]
+        componentes.close()
+    version = ET.SubElement(root, 'version')
+    version.text = '52.0'
+    tree = ET.ElementTree(indent(root))
+    tree.write('packages/package'+name.text+str(xmlVersion)+'.xml', xml_declaration=True, encoding='utf-8')
+    if end >= len(members):
+        return
+    else:
+        createBigXMLs(end,end+30,xmlVersion+1)
+
 # Listado de toda la metadata existente en la Org
 os.system("sfdx force:mdapi:describemetadata -f ./data/meta.json -u "+origen)
 
@@ -51,27 +79,32 @@ os.system("mkdir packages")
 # Creacion del Archivo XML
 for subdir, dirs, files in os.walk('./data'):
     for f in files:
-        root = ET.Element('Package', {'xmlns': 'http://soap.sforce.com/2006/04/metadata'})
-        tipo = f.split('.')
-        tipos = ET.SubElement(root, 'types')
-        componentes = open('./data/'+f)
-        s = componentes.read()
-        members = json.loads(s)
-        if not isinstance(members, list):
-            members = json.loads('['+s+']')
-        not_necessary= ['CustomObjectTranslation.json', 'Workflow.json', 'InstalledPackage.json', 'KeywordLis.json', 'ModerationRule.json', 'WebLink.json', 'WorkflowFieldUpdate.json', 'UserCriteria.json', 'SiteDotCom.json', 'SharingRules.json', 'SharingGuestRule.json', 'Report.json','meta.json']
-        if not f in not_necessary:
-        # if f != 'meta.json'
-            for j in members:
-                metadata = ET.SubElement(tipos, 'members')
-                metadata.text = j['fullName']
-            name = ET.SubElement(tipos, 'name')
-            name.text = tipo[0]
-            componentes.close()
-        version = ET.SubElement(root, 'version')
-        version.text = '52.0'
-        tree = ET.ElementTree(indent(root))
-        tree.write('packages/package'+name.text+'.xml', xml_declaration=True, encoding='utf-8')
+        if f == "StaticResource.json":
+            createBigXMLs()
+        else:
+            root = ET.Element('Package', {'xmlns': 'http://soap.sforce.com/2006/04/metadata'})
+            tipo = f.split('.')
+            tipos = ET.SubElement(root, 'types')
+            componentes = open('./data/'+f)
+            s = componentes.read()
+            members = json.loads(s)
+            if not isinstance(members, list):
+                members = json.loads('['+s+']')
+            not_necessary= ['CustomObjectTranslation.json', 'Workflow.json', 'InstalledPackage.json', 'KeywordLis.json', 'ModerationRule.json', 'WebLink.json', 'WorkflowFieldUpdate.json', 'UserCriteria.json', 'SiteDotCom.json', 'SharingRules.json', 'SharingGuestRule.json', 'Report.json','meta.json']
+            if not f in not_necessary:
+            # if f != 'meta.json'
+                for j in members:
+                    metadata = ET.SubElement(tipos, 'members')
+                    metadata.text = j['fullName']
+                name = ET.SubElement(tipos, 'name')
+                name.text = tipo[0]
+                componentes.close()
+            version = ET.SubElement(root, 'version')
+            version.text = '52.0'
+            tree = ET.ElementTree(indent(root))
+            tree.write('packages/package'+name.text+'.xml', xml_declaration=True, encoding='utf-8')
+        
+
 
 ########## comentar desde de aqui si solo se buscan los XML#################################
 os.system("mkdir unpackages")
